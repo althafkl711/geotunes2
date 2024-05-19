d4c8f56bc4812be7b3b23eed4ce651f5baf8a3a2
althafkl711 <113635353+althafkl711@users.noreply.github.com>
2024-05-19 14:28:59 +0530

    Second Commit

--- AudioArtistry/settings.py
+++ AudioArtistry/settings.py
@@ -136,6 +136,9 @@ CORS_ALLOWED_ORIGINS = [
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'AudioArtistryFE/dist/assets'),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
